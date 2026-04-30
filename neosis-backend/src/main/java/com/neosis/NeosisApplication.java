0f4be973a03e26f9ac80547b951759ce3011332a
Arpit Tagade <143588157+tagadearpit@users.noreply.github.com>
2026-05-01 01:07:17 +0530

    Add

---
+++ neosis-backend/src/main/java/com/neosis/NeosisApplication.java
@@ -0,0 +1,13 @@
+package com.neosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeosisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeosisApplication.class, args);
+    }
+
+}
