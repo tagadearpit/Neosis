f0eba11e5310da19b4fae1483bc4db5d8d069c55
Arpit Tagade <143588157+tagadearpit@users.noreply.github.com>
2026-05-15 00:44:31 +0530

    cdasc

--- neosis-frontend/tailwind.config.js
+++ neosis-frontend/tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class', // This tells Tailwind to enable dark mode via a CSS class!
   theme: {
     extend: {},
   },
